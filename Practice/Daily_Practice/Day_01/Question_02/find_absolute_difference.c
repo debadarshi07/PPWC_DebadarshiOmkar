111eee1b18bda72b7ff6171f337f302ca8ed9118
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2024-10-28 21:01:04 +0530

    Solve Question_2

---
+++ Practice/Daily_Practice/Day_01/Question_02/find_absolute_difference.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    float x, y;
+    printf("Enter the value of x and y separating by a space: ");
+    scanf("%f %f", &x, &y);
+
+    y = x > y ? x - y : y - x ;
+    
+    printf("The difference between x and y is: %.2f\n", y);
+    return 0;
+}
