d067b9507114329dd53441db20a337b2d0f4f406
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2024-11-07 03:13:26 +0530

    Testing increment decrement in pointers

--- Practice/Practice_Sessions/TestCode.c
+++ Practice/Practice_Sessions/TestCode.c
@@ -5,25 +5,64 @@ int main () {
 	int x = 20;
 	int *ptr = &x;
 	
-	printf("x: %d\n\n", x);
-	printf("&x: %u\n\n", &x);
-	printf("ptr: %u\n\n", ptr);
-	printf("*ptr: %d\n\n", *ptr);
-	printf("*(&x): %d\n\n", *(&x));
-	printf("&ptr: %u\n\n", &ptr);
-	printf("*(&ptr): %u\n\n", *(&ptr));
-	printf("(&ptr): %d\n\n", **(&ptr));
+	printf("\nTesting pointers with different combinations of operators :-->\n\n");
+
+	printf("x: %d\n", x);
+	printf("&x: %u\n", &x);
+	printf("ptr: %u\n", ptr);
+	printf("*ptr: %d\n", *ptr);
+	printf("*(&x): %d\n", *(&x));
+	printf("&ptr: %u\n", &ptr);
+	printf("*(&ptr): %u\n", *(&ptr));
+	printf("**(&ptr): %d\n", **(&ptr));
     
     // Pre and Post increment Operators testing
     int *ptr1 = &x, *ptr2 = &x, *ptr3 = &x, *ptr4 = &x;
 
-    printf("*ptr1: %u\n\n", *ptr1);
+	printf("\nPre and Post increment operators testing :-->\n\n");
+
+	printf("ptr1: %u\n", ptr1);
+	printf("ptr2: %u\n", ptr2);
+	printf("ptr3: %u\n", ptr3);
+	printf("ptr4: %u\n\n", ptr4);
+
+	printf("ptr1++: %u\n", ptr1++);
+	printf("ptr1: %u\n", ptr1);ptr1--;
+	printf("ptr2++: %u\n", ptr2++);
+	printf("ptr2: %u\n\n", ptr2);ptr2--;
+
+	printf("++ptr3: %u\n", ++ptr3);
+	printf("ptr3: %u\n", ptr3);--ptr3;
+	printf("++ptr4: %u\n", ++ptr4);
+	printf("ptr4: %u\n\n", ptr4);--ptr4;
+
+    printf("*ptr1: %u\n", *ptr1);
     printf("*ptr2: %u\n\n", *ptr2);
 
-    printf("*ptr1++: %u\n\n", *ptr1++);
-    printf("*(ptr2++): %u\n\n", *(ptr2++));
+    printf("*ptr1++: %u\n", *ptr1++);
+	printf("ptr1: %u\n", ptr1);
+	printf("*ptr1: %d\n", *ptr1);
+    printf("*(ptr2++): %u\n", *(ptr2++));
+	printf("ptr2: %u\n", ptr2);
+	printf("*ptr2: %d\n\n", *ptr2);
 
-    printf("*++ptr3: %u\n\n", *++ptr3);
-    printf("*(++ptr4): %u\n\n", *(++ptr4));
+	printf("*ptr3: %u\n", *ptr3);
+    printf("*ptr4: %u\n\n", *ptr4);
+
+    printf("*++ptr3: %u\n", *++ptr3);
+	printf("ptr3: %u\n", ptr3);
+	printf("*ptr3: %d\n", *ptr3);
+    printf("*(++ptr4): %u\n", *(++ptr4));
+	printf("ptr4: %u\n", ptr4);
+	printf("*ptr4: %d\n\n", *ptr4);
+
+	// This is to increment the value inside the pointer
+	printf("(*ptr1)++: %d\n", (*ptr1)++);
+	printf("*ptr1: %d\n", *ptr1);
+	printf("(*ptr2)++: %d\n", (*ptr2)++);
+	printf("*ptr2: %d\n\n", *ptr2);
+	
+	printf("*ptr3: %d\n", *ptr3);
+	printf("*ptr4: %d\n", *ptr4);
     return 0;
 }
