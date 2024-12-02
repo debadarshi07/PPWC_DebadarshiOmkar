4fb3660a69d8b6a82ec4eb751d93de28091c5f02
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2024-12-03 00:24:32 +0530

    Practice

---
+++ Practice/Daily_Practice/2024-12-03/Question_1.c
@@ -0,0 +1,9 @@
+/*
+    Faster access to non local variables is achieved using an array of pointers to activation records called a
+    (a) stack
+    (b) heap
+    (c) display
+    (d) activation tree
+    
+    Answer - (c) display
+*/
