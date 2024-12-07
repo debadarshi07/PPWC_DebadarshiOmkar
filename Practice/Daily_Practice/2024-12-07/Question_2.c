0614b7bdce0bdd4b4c392a544e2f6c3969b069b0
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2024-12-07 23:58:48 +0530

    Practice

---
+++ Practice/Daily_Practice/2024-12-07/Question_2.c
@@ -0,0 +1,12 @@
+/*
+    Consider the following declaration of a two dimensional array in C:
+    char a[100][100];
+    Assuming that the main memory is byte addressable and that the array is stored starting from memory address 0, the address of a[40][50] is:
+
+    (a) 4040
+    (b) 4050
+    (c) 5040
+    (d) 5050
+
+    Answer - (b) 4050
+*/
