e1bacae7f65015913c870d087a347b4fffffec89
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2024-12-11 23:59:08 +0530

    Process ID

---
+++ Classworks/Lessons/Test16.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+	printf("I am a process - %ld\n", (long)getpid());
+	printf("My parent is - %ld\n", (long)getppid());
+	sleep(500);
+	return 0;
+}
