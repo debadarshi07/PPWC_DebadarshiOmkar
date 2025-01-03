d7fd9cad61df611fb4578e8f3bb8c84d0d7c8f8d
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2025-01-04 02:25:46 +0530

    Revise functions in C

--- Practice/Practice_Sessions/05_functions.c
+++ Practice/Practice_Sessions/05_functions.c
@@ -4,8 +4,25 @@ int sum (int a , int b) {
     return a + b;
 }
 
+// Pass by value
+void change_by_value(int a) {
+    a = 6;
+}
+
+// Pass by reference
+void change_by_reference(int *a) {
+    *a = 9;
+}
+
 int main () {
-    printf("%d", sum(5, 8));
+    printf("Addition of given values: %d\n", sum(5, 8));
+
+    int num = 15;
+    change_by_value(num);
+    printf("After changing by value num: %d\n", num);
+
+    change_by_reference(&num);
+    printf("After changing by reference num: %d\n", num);
 
     return 0;
 }
