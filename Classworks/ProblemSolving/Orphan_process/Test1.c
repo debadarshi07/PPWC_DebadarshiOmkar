14f050b03b047be0ee8bff7727f95c779cb1aa81
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2025-01-04 22:33:12 +0530

    Orphan Process in unix

---
+++ Classworks/ProblemSolving/Orphan_process/Test1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main() {
+    pid_t id;
+    printf("Parent Process : Executed by parent process before fork() - PID = (%d)\n", getpid());
+    id = fork();
+    if (id < 0) {
+        printf("\nfork failed\n");
+        exit(-1);
+    }
+    if (id > 0) {
+        printf("\nParent process exited\n");
+        exit(0);
+    }
+    else {
+        printf("\nChild process executing\n");
+        sleep(10);
+        printf("\nI am child process, id value is (%d)\n", id);
+        printf("\nchild process id is (%d)\n", getpid());
+    }
+    return 0;
+}
