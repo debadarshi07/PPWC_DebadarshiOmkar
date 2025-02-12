19d32018f3b647a9375d56d0e3afb22818a11ef2
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2025-02-12 22:46:26 +0530

    Practice

---
+++ Practice/Daily_Practice/2025-02-12/Test1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// Multiple recursion
+void print(int n) {
+    if (n <= 0) return;
+    print(n - 1);
+    printf("%d", n);
+    print(n - 1);
+}
+
+int main() {
+    print(3);
+    return 0;
+}
