c7f86bf8e907eaf90d7d5d4eb3fc8e3cf364e83c
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2025-02-15 23:35:13 +0530

    Practice

--- Practice/Practice_Sessions/TestCode3.c
+++ Practice/Practice_Sessions/TestCode3.c
@@ -37,5 +37,45 @@ int main() {
     int x, y, z;
     int sum = (x = 5, y = 3, z = 8, x + y + z);
     printf("sum: %d\n\n", sum);
+
+    // This always assigns the later most value to the variable.
+    int n = (10, 20);
+    printf("n: %d\n\n", n);
+
+    // We need to find the rightmost question mark and that should be matched with the very next colon.
+    // Evaluation starts from 5 > 4, then if it's true then only the expression 6 > 7 will be calculated.
+    // Personal trick: Try to find consecutive ? or : (then group from that position to both the sides)
+    int ans = 5 > 4 ? 6 > 7 ? 10 : 20 : 30;
+    printf("ans: %d\n\n", ans);
+
+    // Shortcircuit code: Once the first ternary expression is found false it doesn't execute the truth portion.
+    int h = 8, k = 9;
+    int ans1 = 5 < 4 ? 6 > 7 ? ++h : 20 : 30;
+    int ans2 = 5 < 4 ? (6 > 7 ? ++h : 20) : 30;
+
+    int ans3 = 5 > 4 ? 6 > 7 ? 10 : 20 : ++k;
+    int ans4 = 5 > 4 ? (6 > 7 ? 10 : 20) : ++k;
+
+    printf("h: %d, ans1: %d\n", h, ans1);
+    printf("h: %d, ans2: %d\n", h, ans2);
+    printf("k: %d, ans3: %d\n", k, ans3);
+    printf("k: %d, ans4: %d\n\n", k, ans4);
+
+    int n1 = 30, n2 = 10, n3 = 40, n4 = 20;
+
+    int max1 = n1 > n2 ? n1 > n3 ? n1 : n3 : n2 > n3 ? n2 : n3;
+    int max2 = n1 > n2 && n1 > n3 ? n1 : n2 > n3 ? n2 : n3;
+    printf("max1: %d\n", max1);
+    printf("max2: %d\n\n", max2);
+
+    int min1 = n1 < n2 ? n1 < n3 ? n1 < n4 ? n1 : n4 : n3 < n4 ? n3 : n4 : n2 < n3 ? n2 < n4 ? n2 : n4 : n3 < n4 ? n3 : n4;
+    int min2 = n1 < n2 && n1 < n3 && n1 < n4 ? n1 : n2 < n3 && n2 < n4 ? n2 : n3 < n4 ? n3 : n4;
+
+    printf("min1: %d\n", min1);
+    printf("min2: %d\n\n", min2);
+
+    // This is considered as bad programming. O/P changes from compiler to compiler.
+    int s = 5;
+    printf("%d %d %d\n\n", ++x, ++x, ++x);
     return 0;
 }
