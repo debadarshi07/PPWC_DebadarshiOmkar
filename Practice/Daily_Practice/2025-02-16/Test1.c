74f35d4e2361786e431cd0270b4d15803ce5ce75
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2025-02-16 22:23:50 +0530

    Practice

---
+++ Practice/Daily_Practice/2025-02-16/Test1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int count1(int n) {
+    static int x = 10;
+    return x + n;
+}
+
+int count(int n) {
+    static int d = 1;
+    d++;
+    if(n > 1)
+        count(n - 1);
+    d++;
+    return count1(d);
+}
+
+int main() {
+    printf("%d", count(3) + count(4));
+    return 0;
+}
