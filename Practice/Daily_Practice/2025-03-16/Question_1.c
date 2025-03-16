2223b94191186b524897f6238c12e3515e087f36
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2025-03-16 11:22:44 +0530

    Practice

---
+++ Practice/Daily_Practice/2025-03-16/Question_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    double a[2] = {20.0, 25.0}, *p, *q;
+    p = a;
+    q = p + 1;
+    printf("%d, %d", (int)(q - p), (int)(*q - *p));
+    return 0;
+}
+
+/*
+    O/P
+    1, 5
+*/
