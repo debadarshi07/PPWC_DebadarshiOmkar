e2286696680b675cdfb44a8c7dd4569c1057445f
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2025-03-17 20:22:33 +0530

    Practice

---
+++ Practice/Daily_Practice/2025-03-17/Question_1.c
@@ -0,0 +1,7 @@
+/*
+
+Given array A[-5....100], Base Address = 1000 and size of each element is 2 Bytes. What's the address of A[78] ?
+
+Ans). A[78] = 1000 + ((78 - (-5)) * 2) = 1000 + (83 * 2) = 1166
+
+*/
