90f4059e2100c2ce404c98ce8883f4972243974b
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2025-03-18 20:01:12 +0530

    Practice

---
+++ Practice/Daily_Practice/2025-03-18/Question_1.c
@@ -0,0 +1,12 @@
+/*
+
+Consider the following array A[-23....93][-7....31]. Base address is 1000 and each element occupies 2-Bytes of space. Which of the following is TRUE ?
+
+a). The number of rows is 117.
+b). The array B[0...116][2...40] have same number of elements as the array declared in question.
+c). Address of A[0][5] is 3538 row major order.
+d). Address of A[0][0] is 3538 row major order.
+
+Ans). (a), (b)
+
+*/
