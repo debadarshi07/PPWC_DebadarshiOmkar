46f23bd757bbaeba2c11556ed00fcf2bd0a0690a
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2025-03-21 01:13:01 +0530

    Practice

---
+++ Practice/Daily_Practice/2025-03-21/Question_2.c
@@ -0,0 +1,10 @@
+/*
+
+Q). Consider the LTM consists of 45 non zero elements. If the dimension is n * n, then the value of n is _____.
+
+Ans). n(n + 1) = 45 * 2
+      => n(n + 1) = 90
+      => 9 * 10 = 90
+      Hence, n = 9
+
+*/
---
+++ Practice/Daily_Practice/2025-03-21/Question_3.c
@@ -0,0 +1,6 @@
+/*
+
+Q). If natural numbers are stored in array A[-10...10][20...30], then the number stored in A[5][15] is _____.
+
+Ans). After bound adjustment, array will become A[1...21][1...11] and element will become A[16][-4].
+*/
