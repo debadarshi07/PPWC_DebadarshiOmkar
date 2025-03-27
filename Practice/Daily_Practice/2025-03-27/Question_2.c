e1ec9e369a154bc480a269276e78151eebfd5e5a
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2025-03-27 22:42:36 +0530

    Practice

---
+++ Practice/Daily_Practice/2025-03-27/Question_2.c
@@ -0,0 +1,14 @@
+/*
+
+Q). The following postfix expression with single digit operands is evaluated using a stack:
+        8 2 3 ^ / 2 3 * + 5 1 * -
+    The top two elements of the stack after the first * is evaluated are _______
+
+    (a). 6, 1
+    (b). 5, 7
+    (c). 3, 2
+    (d). 1, 5
+
+Ans). (a)
+
+*/
---
+++ Practice/Daily_Practice/2025-03-27/Question_3.c
@@ -0,0 +1,11 @@
+/*
+
+Q). Consider the following expression.
+    (5 2 * 3 3 2 + * +) - (+ * 5 2 * + 3 4 * 5 2)
+
+    What is the result after evaluating the expression where the sub expression in postfix and prefix form and all numbers are single digit numbers
+    (a). 250        (b). -55        (c). 80         (d). 1000
+
+Ans). (c)
+
+*/
