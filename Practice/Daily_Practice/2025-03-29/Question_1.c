d1a2d7f4d2b815103de40b7e8c269ba8b7892a04
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2025-03-29 02:23:09 +0530

    Practice

---
+++ Practice/Daily_Practice/2025-03-29/Question_1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int a[5] = {1, 2, 3, 4, 5};
+    int *ptr = (int*) (&a + 1);
+    printf("%d%d", *(a + 1), *(ptr - 1));
+    return 0;
+}
+
+/*
+    O/P
+    25
+*/
