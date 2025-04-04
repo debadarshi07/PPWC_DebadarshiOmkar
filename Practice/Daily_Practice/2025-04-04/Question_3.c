82023aeb4850a61ded43a26f1afdf2bb68434c18
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2025-04-04 23:59:23 +0530

    Practice

---
+++ Practice/Daily_Practice/2025-04-04/Question_3.c
@@ -0,0 +1,12 @@
+/*
+
+For given preorder : ABCDEFG
+           inorder : BDCAFGE
+
+    What is the postorder of a binary tree ?
+    (a). CDBGFEA            (b). DBCGFEA
+    (c). DCBGFEA            (d). None of these
+
+    Ans). (c)
+
+*/
