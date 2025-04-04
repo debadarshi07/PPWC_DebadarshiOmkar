ac1ee53d424b04ae6d28b924fcf7df629e333953
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2025-04-04 23:57:24 +0530

    Practice

---
+++ Practice/Daily_Practice/2025-04-04/Question_2.c
@@ -0,0 +1,9 @@
+/*
+
+Q). If inorder traversal -> 4, 7, 5, 6, 11, 2, 1, 5, 9
+      preorder traversal -> 2, 7, 4, 6, 5, 11, 5, 1, 9
+    Then sum of internal nodes is _______.
+
+    Ans). 20
+
+*/
