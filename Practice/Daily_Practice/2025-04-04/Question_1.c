10c0ffd21541ec1c0e046b024598d4db0ce3140f
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2025-04-04 23:38:31 +0530

    Practice

---
+++ Practice/Daily_Practice/2025-04-04/Question_1.c
@@ -0,0 +1,9 @@
+/*
+
+Q). If inorder traversal -> 2, 7, 5, 6, 11, 1, 9, 5, 8
+     postorder traversal -> 2, 5, 11, 6, 7, 5, 8, 9, 1
+    Then sum of elements in leaf nodes is _______.
+
+    Ans). 23
+
+*/
