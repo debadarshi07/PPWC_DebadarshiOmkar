1312641f16c458b8ba76736056ce5573bcc70b56
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2025-04-11 22:00:16 +0530

    Practice

---
+++ Practice/Daily_Practice/2025-04-11/Question_4.c
@@ -0,0 +1,9 @@
+/*
+
+Q). Suppose a BST with 37 distinct elements is also a CBT. The tree is stored using the array representation of binary heap trees. Assuming that the array indices start with the 1. Largest element of the tree is stored at index X and smallest element is stored at index Y, then X + Y is _____.
+
+Ans). Index of smallest element = 32
+      Index of largest element = 31
+      X + Y = 31 + 32 = 63
+
+*/
