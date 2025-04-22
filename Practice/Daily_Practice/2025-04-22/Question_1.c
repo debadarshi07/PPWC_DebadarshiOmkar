104809c06d8b47b231699d95c5ce1018fc5929d1
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2025-04-22 19:00:30 +0530

    Practice

---
+++ Practice/Daily_Practice/2025-04-22/Question_1.c
@@ -0,0 +1,15 @@
+/*
+
+Q). The most appropriate matching for the following pairs.
+        X: DFS          1: heap
+        Y: BFS          2: queue
+        Z: Sorting      3: stack
+
+    (a). X-1 Y-2 Z-3
+    (b). X-3 Y-1 Z-2
+    (c). X-3 Y-2 Z-1
+    (d). X-2 Y-3 Z-1
+
+    Ans). (c)
+
+*/
---
+++ Practice/Daily_Practice/2025-04-22/Question_2.c
@@ -0,0 +1,12 @@
+/*
+
+Q). Let LASTPOST, LASTIN, LASTPRE denote the last vertex visited in postorder, inorder and preorder traversal respectively, of a CBT. Which of the following is always true ?
+
+(a). LASTIN = LASTPOST
+(b). LASTIN = LASTPRE
+(c). LASTPRE = LASTPOST
+(d). None of the above
+
+Ans). (d)
+
+*/
