3da8ab1bafd38850b65267f0d75accde25d2ccd2
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2025-05-11 15:06:57 +0530

    Practice

---
+++ Practice/Daily_Practice/2025-05-11/Question_1.c
@@ -0,0 +1,13 @@
+/*
+
+Q). What are the min & max number of comparisons needed in 2-way merging ?
+    L1 -> m, L2 -> n & m <= n
+
+(a). m, m+n
+(b). n, m+n-1
+(c). m, m+n-1
+(d). n, m+n
+
+Ans). (c)
+
+*/
