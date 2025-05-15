d856dc823afc2a2d1adea7a5e397568929f14806
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2025-05-15 23:59:37 +0530

    Practice

---
+++ Practice/Daily_Practice/2025-05-15/Question_1.c
@@ -0,0 +1,14 @@
+/*
+
+Q). If one uses straight two way merge sort algorithm to sort the following elements is ascending order.
+    20, 47, 15, 8, 9, 4, 40, 30, 12, 17
+    Then the order of these elements after second pass of algorithm is
+
+    (a). 8, 9, 15, 20, 47, 4, 12, 17, 30, 40
+    (b). 8, 15, 20, 47, 4, 9, 30, 40, 12, 17
+    (c). 15, 20, 47, 4, 8, 9, 12, 30, 40, 17
+    (d). 4, 8, 9, 15, 20, 47, 12, 17, 30, 40
+
+    Ans). (b)
+
+*/
