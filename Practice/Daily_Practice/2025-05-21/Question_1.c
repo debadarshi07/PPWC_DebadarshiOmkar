f0317b12efd577f53d5ea2a93f76e2170075441f
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2025-05-21 22:33:12 +0530

    Practice

---
+++ Practice/Daily_Practice/2025-05-21/Question_1.c
@@ -0,0 +1,17 @@
+/*
+
+Q). Find maximum optimal solution to the fractional knapsack problem:
+    Given, n = 7, M = 15
+
+    Object          Profit          Weight
+    1               10              2
+    2               5               3
+    3               15              5
+    4               7               7
+    5               6               1
+    6               18              4
+    7               3               1
+
+Ans). 55.33
+
+*/
---
+++ Practice/Daily_Practice/2025-05-21/Question_2.c
@@ -0,0 +1,15 @@
+/*
+
+Q). Consider the weights and values of items listed below. Note that there is only one unit of each item.
+
+    item            weight               Value
+    1                 10                  60
+    2                  7                  28
+    3                  4                  20
+    4                  2                  24
+
+    The task is to pick a subset of these items such that their total weight is no more than 11 kgs and their total value is maximized. Moreover, no item may be split. The total value of items picked by an optimal algorithm is denoted by Vopt. A greedy algorithm sorts the items by their value to weight ratios in descending order and packs them greedily, starting from the first item in the ordered list. The total value of items picked by the greedy algorithm is denoted by Vgreedy. The value of Vopt - Vgreedy is _____.
+
+    Ans). 16
+
+*/
