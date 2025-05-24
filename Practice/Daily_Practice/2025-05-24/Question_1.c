4d53da5402958e85404869cd093e369ff2985a8e
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2025-05-24 21:12:21 +0530

    Practice

---
+++ Practice/Daily_Practice/2025-05-24/Question_1.c
@@ -0,0 +1,17 @@
+/*
+
+Q). Consider the string abbccddeee. Each letter in the string must be assigned a binary code satisfying the following properties:
+
+For any two letters, the code assigned to one letter must not be a prefix of the code assigned to the other letter.
+For any two letters of the same frequency, the letter which occurs earlier in the dictionary order is assigned a code whose length is at most the length of the code assigned to the other letter.
+
+Among the set of all binary code assignments which satisfy the above two properties, what is the minimum length of the encoded string?
+
+(a). 21
+(b). 23
+(c). 25
+(d). 30
+
+Ans). (b)
+
+*/
