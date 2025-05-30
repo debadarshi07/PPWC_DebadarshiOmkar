4a5fa7859599e53b22bc25bdcec8ad26c0163654
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2025-05-30 13:18:29 +0530

    Practice

---
+++ Practice/Daily_Practice/2025-05-30/Question_1.c
@@ -0,0 +1,12 @@
+/*
+
+Q). G = (V,E) is an undirected simple graph in each edge has a distinct weight, and e is particular edge of G. Which of the following statement about the minimum spanning tree(MSTs) of G is/are True ?
+
+    I. If e is the highest edge of some cycle in G, then every MST of G includes e.
+    II. If e is the heaviest edge of some cycle in G, then every MST of G excludes e.
+
+(a). I only      (b). II only       (c). Both I and II          (d). Neither I nor II
+
+Ans). (b)
+
+*/
---
+++ Practice/Daily_Practice/2025-05-30/Question_2.c
@@ -0,0 +1,13 @@
+/*
+
+Q). Let G be a connected undirected weighted graph. Consider the following two statements.
+
+    S1: There exists a minimum weight edge in G which is present in every minimum spanning tree of G.
+    S2: If every edge in G has distinct weight, then G has a unique minimum spanning tree.
+
+(a). Both S1 and S2 are true                (b). S1 is true and S2 is false
+(c). S1 is false and S2 is true             (d). Both S1 and S2 are false
+
+Ans). (c)
+
+*/
