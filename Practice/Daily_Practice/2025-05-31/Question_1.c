7ee6f975f32880e3a4354fe70a207dd33f95d448
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2025-05-31 13:38:12 +0530

    Practice

---
+++ Practice/Daily_Practice/2025-05-31/Question_1.c
@@ -0,0 +1,10 @@
+/*
+
+Q). Consider a weighted undirected graph with positive edge weights and let uv be an edge in the graph. It is known that the shortest path from the sources vertex s to u has weight 53 and the shortest path from s to v has weight 65. Which one of the following statements is always true ?
+
+(a). Weight(u,v) < 12               (b). Weight(u,v) <= 12
+(c). Weight(u,v) > 12               (d). Weight(u,v) >= 12
+
+Ans). (d)
+
+*/
