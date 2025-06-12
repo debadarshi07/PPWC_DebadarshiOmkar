ccbb06bd9ac3b25b429ccbc2980a3a7a95ec0648
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2025-06-13 00:12:43 +0530

    Practice

---
+++ Practice/Daily_Practice/2025-06-13/Question_2.c
@@ -0,0 +1,10 @@
+/*
+
+Q). Which one is valid 3-ary max heap array representation ?
+
+(a). <1, 3, 5, 6, 8, 9>         (b). <9, 6, 3, 1, 8, 5>
+(c). <9, 3, 6, 8, 5, 1>         (d). <9, 5, 6, 8, 3, 1>
+
+Ans). (d)
+
+*/
---
+++ Practice/Daily_Practice/2025-06-13/Question_3.c
@@ -0,0 +1,9 @@
+/*
+
+Q). In a binary Max-Heap with n elements, the smallest element can be found in time of _____.
+
+(a). O(nlogn)       (b). O(n)       (c). O(n²)      (d). O(1)
+
+Ans). (b)
+
+*/
