d6a0528eb432c817c43d1bf7adb0e22168748e86
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2025-06-16 22:35:27 +0530

    Practice

--- Practice/Daily_Practice/2025-06-16/Question_1.c
+++ Practice/Daily_Practice/2025-06-16/Question_1.c
@@ -1,5 +1,9 @@
 /*
 
-Q). 
+Q). The average number of key comparisons required for a successful search for sequential search on n items is
+
+(a). n / 2          (b). (n - 1) / 2            (c). (n + 1) / 2            (d). None of the above
+
+Ans). (c)
 
 */
