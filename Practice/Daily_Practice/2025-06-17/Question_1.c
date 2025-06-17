866188a392ae12f82ba324060ca126f66beb48c4
Debadarshi Omkar <debadarshi.omkar08@gmail.com>
2025-06-17 22:36:58 +0530

    Practice

---
+++ Practice/Daily_Practice/2025-06-17/Question_1.c
@@ -0,0 +1,7 @@
+/*
+
+Q). For n distict elements, how many binary search trees are possible ?
+
+Ans). catalan number
+
+*/
---
+++ Practice/Daily_Practice/2025-06-17/Question_2.c
@@ -0,0 +1,14 @@
+/*
+
+Q). Given, A = [a1, a2, a3] = [do, if, while]
+               [p1, p2, p3] = [0.5, 0.1, 0.05]
+               [q0, q1, q2, q3] = [0.15, 0.1, 0.05, 0.05]
+
+Find the cost for the given binary search tree represented by children list below.
+        if = [do, while]
+        do = [E0, E1]
+        while = [E2, E3]
+
+Ans). 1.9
+
+*/
